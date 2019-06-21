8588e2ac179e683faa3c0a3e05ec064b12974822
xkopec17 <xkopec17@mendelu.cz>
2019-06-21 12:52:38 +0200

    test

---
+++ app/src/main/java/com/example/robinkopecky/pizza/IntentConstants.java
@@ -0,0 +1,6 @@
+package com.example.robinkopecky.pizza;
+
+public class IntentConstants {
+
+    public static final String ID = "id";
+}
---
+++ app/src/main/java/com/example/robinkopecky/pizza/PizzaDetailActivity.java
@@ -0,0 +1,38 @@
+package com.example.robinkopecky.pizza;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PizzaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public static Intent getIntent(MainActivity context, Long id) {
+        Intent intent = new Intent(context,
+                PizzaDetailActivity.class);
+        if (id != null){
+            intent.putExtra(IntentConstants.ID, id);
+        }
+        return intent;
+    }
+}
